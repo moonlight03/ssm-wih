e385828c345e14af99071d8a192fe774f121465e
lixiang <1739781578@qq.com>
2019-06-10 23:34:26 +0800

    123

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -4,15 +4,12 @@ import com.example.entity.User;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * @Author:wjup
  * @Date: 2018/9/26 0026
  * @Time: 14:42
  */
- 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -23,18 +20,13 @@ public class UserController {
     @RequestMapping("getUser/{id}")
     public String GetUser(@PathVariable int id){
         System.out.println();
+
         return userService.Sel(id).toString();
     }
-    @GetMapping("/getUserById")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public User getUserById(@RequestParam(value = "id") String id,
                             @RequestParam(value = "pass") String pass){
-        System.out.println(id);
+
         return userService.getUserById(Integer.parseInt(id),pass);
-    }
-    @GetMapping("/sgetUserById")
-    public User getUserById(HttpServletRequest request){
-        String id = request.getParameter("id");
-        System.out.println(id);
-        return userService.getUserById(Integer.parseInt(id),"123456");
     }
 }

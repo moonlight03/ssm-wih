e8cc384b270402db6951cbcd70aa2c8ac5faf78a
1449787979@qq.com <1449787979@qq.com>
2019-12-28 22:27:24 +0800

    Job

--- src/main/java/com/example/entity/Job.java
+++ src/main/java/com/example/entity/Job.java
@@ -1,5 +1,38 @@
 package com.example.entity;
 
+import java.util.Date;
 public class Job {
+    private Integer jid;
+    private String context;
+    private Date deadline;
 
+    public Integer getJid() {
+        return jid;
+    }
+
+    public void setJid(Integer jid) {
+        this.jid = jid;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Job(Integer jid, String context, Date deadline) {
+        this.jid = jid;
+        this.context = context;
+        this.deadline = deadline;
+    }
 }
